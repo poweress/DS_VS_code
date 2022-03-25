801256fdff2ac7bab390fd4e9666484b3922da8c
poweress <63856450+poweress@users.noreply.github.com>
2022-03-25 12:25:25 +0600

    готов

--- games/main.py
+++ games/main.py
@@ -2,9 +2,7 @@
 Компьютер сам загадывает и сам угадывает число
 """
 
-
 import numpy as np
-
 
 def random_predict(number: int = 1) -> int:
     """Рандомно угадываем число
@@ -19,18 +17,16 @@ def random_predict(number: int = 1) -> int:
     min, max = 1, 101 # Определим минимум и максимум
     number_predict = int(max / 2) # Угадываемое число - максимальное значение делим по полам
     
-    # 
-    while number != number_predict:
-        
-        count += 1           
-             
-        if number > number_predict:
-            min = number_predict
-            number_predict = int((max + min) / 2)
-        elif number < number_predict:
-            max  = number_predict
-            number_predict = int((max + min) / 2)
-                        
+    # Во вложенных циклах  будем сужать диапазон минимального и максимально значения
+    while number != number_predict:        
+        count += 1  # Увеличиваем счетчик попыток на 1                      
+        if number > number_predict: #  Если истина, определим минимум
+            min = number_predict # Запишим минимальное значение 
+            number_predict = int((max + min) / 2) # Половиним угадываемое число на два
+        elif number < number_predict: #  Если истина, определим максимум
+            max  = number_predict # Запишем максимальное значение
+            number_predict = int((max + min) / 2) #  И снова половиним угадываемое число
+            #  Повторим цикл еще раз            
             while number != number_predict:    
                 if number > number_predict:
                     min = number_predict
@@ -38,7 +34,7 @@ def random_predict(number: int = 1) -> int:
                 elif number <number_predict:
                     max = number_predict
                     number_predict = int((max + min) / 2)                    
-                    
+                    # И еще раз повторим
                     while number != number_predict:
                         if number > number_predict:
                             min = number_predict
@@ -46,7 +42,7 @@ def random_predict(number: int = 1) -> int:
                         elif number < number_predict:
                             max = number_predict
                             number_predict = int((max + min) / 2)                            
-                            
+                            # И здесь повторим
                             while number != number_predict:
                                 if number > number_predict:
                                     min = number_predict
@@ -54,7 +50,7 @@ def random_predict(number: int = 1) -> int:
                                 elif number <number_predict:
                                     max = number_predict
                                     number_predict = int((max + min) / 2)                                    
-                                    
+                                    #  И здесь 
                                     while number != number_predict:
                                         if number > number_predict:
                                             min = number_predict
@@ -62,7 +58,7 @@ def random_predict(number: int = 1) -> int:
                                         elif number <number_predict:
                                             max = number_predict
                                             number_predict = int((max + min) / 2)                                            
-                                            
+                                            #  аналогично как и выше 
                                             while number != number_predict:
                                                 if number > number_predict:
                                                     min = number_predict
@@ -70,11 +66,7 @@ def random_predict(number: int = 1) -> int:
                                                 elif number < number_predict:
                                                     max = number_predict
                                                     number_predict = int((max + min) / 2)
-                                                    
-                                                
-                   
     return count
-
 
 def score_game(random_predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
@@ -88,11 +80,11 @@ def score_game(random_predict) -> int:
     count_ls = []
     #np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
-
+    #  При каждой итэрации будем добовлять загаданное число в список count_ls
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(random_predict(number)) 
 
-    score = int(np.mean(count_ls))
+    score = int(np.mean(count_ls)) # Запишем среднее значение
     print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
     return score
 
